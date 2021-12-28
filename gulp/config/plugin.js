58e1a24db3432ed6287f0a5bf50b537e9208b479
Valentin <Konopasies@yandex.by>
2021-12-28 23:45:41 +0300

    install and setting  gulp-plumber gulp-notify

--- gulp/config/plugin.js
+++ gulp/config/plugin.js
@@ -1,5 +1,9 @@
-import replace from "gulp-replace" //Поиск и замена
+import replace from "gulp-replace"; // Поиск и замена
+import plumber from "gulp-plumber"; // Обработка ошибок
+import notify from "gulp-notify"; // Сообщения (подсказки)
 
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber: plumber,
+    notify: notify
 }
