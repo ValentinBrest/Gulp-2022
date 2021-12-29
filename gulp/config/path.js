caa8b3344a2375cfd31752ef468bc5c79b294d0c
Valentin <Konopasies@yandex.by>
2021-12-29 22:03:55 +0300

    font

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -10,6 +10,7 @@ export const path = {
         css: `${buildFolder}/css/`,
         html: `${buildFolder}/`,
         images: `${buildFolder}/img/`,
+        fonts: `${buildFolder}/fonts/`,
         files: `${buildFolder}/files/`
     },
     src: {
@@ -19,6 +20,7 @@ export const path = {
         html: `${srcFolder}/*.html`,
         images: `${srcFolder}/img/**/*.{jpg,jpeg,png,gif,webp}`,
         files: `${srcFolder}/files/**/*.*`,
+        svgicons: `${srcFolder}/svgicons/*.svg`,
     },
     watch: {
         js: `${srcFolder}/js/**/*.js`,
