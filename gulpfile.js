5f8a2283dea09be6a6d439d556037ac11e195ef6
Valentin <Konopasies@yandex.by>
2021-12-29 22:48:06 +0300

    zip

--- gulpfile.js
+++ gulpfile.js
@@ -23,8 +23,7 @@ import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontStyle  } from "./gulp/tasks/fonts.js";
-
-
+import { zip } from "./gulp/tasks/zip.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
@@ -44,10 +43,12 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
+const deployZIP = gulp.series(reset, mainTasks, zip);
 
 //Экспорт сценариев
 export { dev }
 export { build }
+export { deployZIP }
 
 // Выполение сценария по умолчанию
 gulp.task('default', dev);
