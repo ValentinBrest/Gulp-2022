c57ba14cc3fb252a0be5fff0b3052a20dc86e5e5
Valentin <Konopasies@yandex.by>
2021-12-29 11:56:58 +0300

    install gulp-clean-css gulp-webpcss gulp-autoprefixer gulp-group-css-media-queries

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -2,6 +2,11 @@ import dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 import rename from 'gulp-rename';
 
+import cleanCss from 'gulp-clean-css'; // Сжатие CSS файла
+import webpcss from 'gulp-webpcss'; // Вывод WEBP изображений
+import autoprefixer from 'gulp-autoprefixer'; // Добавление вендорных префиксов
+import groupCssMediaQueries from 'gulp-group-css-media-queries'; // Группировка медиа запросов  
+
 const sass = gulpSass(dartSass);
 
 export const scss = () => {
@@ -15,6 +20,11 @@ export const scss = () => {
         .pipe(app.plugins.replace(/@img\//g, '../img/'))
         .pipe(sass({
             outputStyle: 'expanded'
+        }))
+        .pipe(groupCssMediaQueries())
+        .pipe(webpcss({
+            webpClass: ".webp",
+            noWebpClass: ".no-webp"
         }))
         .pipe(rename({
             extname: ".min.css"
