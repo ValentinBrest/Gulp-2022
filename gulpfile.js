52b10bc860bf3ed7de3a3382ddf9b07a1804122a
Valentin <Konopasies@yandex.by>
2021-12-29 23:03:15 +0300

    ftp

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,7 @@ import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontStyle  } from "./gulp/tasks/fonts.js";
 import { zip } from "./gulp/tasks/zip.js";
+import { ftp } from "./gulp/tasks/ftp.js";
 
 // Наблюдатель за изменениями в файлах
 function watcher() {
@@ -44,11 +45,13 @@ const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
 const build = gulp.series(reset, mainTasks);
 const deployZIP = gulp.series(reset, mainTasks, zip);
+const deployFTP = gulp.series(reset, mainTasks, ftp);
 
 //Экспорт сценариев
 export { dev }
 export { build }
 export { deployZIP }
+export { deployFTP }
 
 // Выполение сценария по умолчанию
 gulp.task('default', dev);
